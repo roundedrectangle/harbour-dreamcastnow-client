c33a18beaf8f23d2a3474f6fef8b3689f4926c2e
legacychimera247 <mad_fer_it@vivaldi.net>
2025-11-20 23:46:56 +0100

    Add files via upload

---
+++ translations/harbour-dreamcastnow-client-it.ts
@@ -0,0 +1,166 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="it_IT">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="28"/>
+        <source>Dreamfish Now</source>
+        <translation>Dreamfish Now</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="53"/>
+        <source>This project is licensed under GNU GPL 3.0. Copyright (c) 2025 roundedrectangle</source>
+        <translation>Questo progetto è sotto licenza GNU-GPL 3.0. Copyright (c) 2025 roundedrectangle</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="58"/>
+        <location filename="../qml/pages/AboutPage.qml" line="76"/>
+        <source>GitHub</source>
+        <translation>GitHub</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="64"/>
+        <source>dom-parser</source>
+        <translation>dom parser</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="71"/>
+        <source>This project uses a modified version of the dom-parser project, licensed under ISC.</source>
+        <translation>Questo progetto usa una versione modificata del progetto dom parser, sotto licenza ISC.</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="80"/>
+        <source>ISC License</source>
+        <translation>Licenza ISC</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message numerus="yes">
+        <location filename="../qml/cover/CoverPage.qml" line="41"/>
+        <source>%Ln online</source>
+        <translation>
+            <numerusform>%Ln in linea</numerusform>
+            <numerusform>%Ln in linea</numerusform>
+        </translation>
+    </message>
+</context>
+<context>
+    <name>FirstPage</name>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="27"/>
+        <source>About</source>
+        <translation>Info</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="31"/>
+        <source>Settings</source>
+        <translation>Impostazioni</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="35"/>
+        <source>Reload</source>
+        <translation>Ricarica</translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../qml/pages/FirstPage.qml" line="41"/>
+        <source>%Ln online</source>
+        <translation>
+            <numerusform>%Ln in linea</numerusform>
+            <numerusform>%Ln in linea</numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../qml/pages/FirstPage.qml" line="46"/>
+        <source>Online (%Ln)</source>
+        <translation>
+            <numerusform>In linea (%Ln)</numerusform>
+            <numerusform>In linea (%Ln)</numerusform>
+        </translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="46"/>
+        <source>Offline</source>
+        <translation>Non in linea</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="142"/>
+        <source>Played recently:</source>
+        <translation>Giocati recentemente:</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="17"/>
+        <source>Settings</source>
+        <translation>Impostazioni</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="21"/>
+        <source>Show background</source>
+        <translation>Mostra sfondo titolo</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="27"/>
+        <source>Separators</source>
+        <translation>Separatori</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="29"/>
+        <source>Disabled</source>
+        <translation>Disabilitati</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="30"/>
+        <source>Enabled</source>
+        <translation>Abilitati</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="31"/>
+        <source>Enabled with more spacing</source>
+        <translation>Abilitati con più spazio</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="39"/>
+        <source>Base URL, without a slash in the end</source>
+        <translation>URL base, senza / alla fine</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="56"/>
+        <source>Page path</source>
+        <translation>Percorso pagina</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="72"/>
+        <source>Auto-update</source>
+        <translation>Auto ricarica</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="85"/>
+        <source>Auto-update interval, in seconds: %1</source>
+        <translation>Intervallo auto ricarica di secondi: %1</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="86"/>
+        <source>Auto-update interval, in seconds</source>
+        <translation>Intervallo auto ricarica, in secondi</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="104"/>
+        <source>Auto-update in background</source>
+        <translation>Auto ricarica in background</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="117"/>
+        <source>Background auto-update interval, in seconds: %1</source>
+        <translation>Intervallo auto ricarica in background di secondi: %1</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="118"/>
+        <source>Background auto-update interval, in seconds</source>
+        <translation>Intervallo auto ricarica in background di secondi</translation>
+    </message>
+</context>
+</TS>
