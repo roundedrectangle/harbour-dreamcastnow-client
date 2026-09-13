6ff099caed54569bb785ce77bf46fd401f1bda5f
legacychimera247 <mad_fer_it@vivaldi.net>
2026-09-13 19:41:19 +0200

    italian translation updated

---
+++ translations/harbour-dreamcastnow-client-it.ts
@@ -0,0 +1,210 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="it_IT">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <source>This project is licensed under GNU GPL 3.0. Copyright (c) 2025 roundedrectangle</source>
+        <translation>Questo progetto è sotto licenza GNU-GPL 3.0. Copyright (c) 2025 roundedrectangle</translation>
+    </message>
+    <message>
+        <source>Translations</source>
+        <translation>Traduzioni</translation>
+    </message>
+    <message>
+        <source>Italian</source>
+        <translation>Italiano</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message numerus="yes">
+        <source>%Ln online</source>
+        <translation>
+            <numerusform>%Ln in linea</numerusform>
+            <numerusform>%Ln in linea</numerusform>
+        </translation>
+    </message>
+</context>
+<context>
+    <name>FirstPage</name>
+    <message>
+        <source>About</source>
+        <translation>Info</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Impostazioni</translation>
+    </message>
+    <message>
+        <source>Use Dreamcast Now</source>
+        <translation>Vai a Dreamcast Now</translation>
+    </message>
+    <message>
+        <source>Use DCNet</source>
+        <translation>Vai a DCNet</translation>
+    </message>
+    <message>
+        <source>Refresh</source>
+        <translation>Ricarica</translation>
+    </message>
+    <message numerus="yes">
+        <source>%Ln online</source>
+        <translation>
+            <numerusform>%Ln in linea</numerusform>
+            <numerusform>%Ln in linea</numerusform>
+        </translation>
+    </message>
+    <message>
+        <source>Offline</source>
+        <translation>Non in linea</translation>
+    </message>
+    <message>
+        <source>Playing %1</source>
+        <translation>Sta giocando a %1</translation>
+    </message>
+    <message>
+        <source>Started playing %1</source>
+        <comment>time</comment>
+        <translation>Ha iniziato a giocare da %1</translation>
+    </message>
+    <message>
+        <source>Last seen %1</source>
+        <comment>time</comment>
+        <translation>Visto l&apos;ultima volta %1</translation>
+    </message>
+    <message>
+        <source>Played recently:</source>
+        <translation>Giocati recentemente:</translation>
+    </message>
+    <message>
+        <source>Unknown error</source>
+        <translation>Errore sconosciuto</translation>
+    </message>
+    <message>
+        <source>No players</source>
+        <translation>Non ci sono giocatori</translation>
+    </message>
+    <message>
+        <source>Unexpected HTTP status code</source>
+        <translation>Stato codice HTTP inaspettato</translation>
+    </message>
+    <message>
+        <source>Couldn&apos;t parse the response as JSON</source>
+        <translation>Non posso decodificare la risposta come JSON</translation>
+    </message>
+    <message>
+        <source>Try again by pulling down to refresh</source>
+        <translation>Prova di nuovo trascinando il pulley in basso per ricaricare</translation>
+    </message>
+    <message>
+        <source>Unsubscribe</source>
+        <translation>Disattiva notifica</translation>
+    </message>
+    <message>
+        <source>Subscribe</source>
+        <translation>Attiva notifica</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <source>Settings</source>
+        <translation>Impostazioni</translation>
+    </message>
+    <message>
+        <source>Show background</source>
+        <translation>Mostra sfondo titolo</translation>
+    </message>
+    <message>
+        <source>Separators</source>
+        <translation>Separatori</translation>
+    </message>
+    <message>
+        <source>Disabled</source>
+        <translation>Disabilitati</translation>
+    </message>
+    <message>
+        <source>Enabled</source>
+        <translation>Abilitati</translation>
+    </message>
+    <message>
+        <source>Enabled with more spacing</source>
+        <translation>Abilitati con più spazio</translation>
+    </message>
+    <message>
+        <source>Base URL, without a slash in the end</source>
+        <translation>URL base, senza / alla fine</translation>
+    </message>
+    <message>
+        <source>Page path</source>
+        <translation>Percorso pagina</translation>
+    </message>
+    <message>
+        <source>Auto-update</source>
+        <translation>Auto ricarica</translation>
+    </message>
+    <message>
+        <source>Auto-update interval, in seconds: %1</source>
+        <translation>Intervallo auto ricarica di secondi: %1</translation>
+    </message>
+    <message>
+        <source>Auto-update interval, in seconds</source>
+        <translation>Intervallo auto ricarica, in secondi</translation>
+    </message>
+    <message>
+        <source>Auto-update in background</source>
+        <translation>Auto ricarica in background</translation>
+    </message>
+    <message>
+        <source>Background auto-update interval, in seconds: %1</source>
+        <translation>Intervallo auto ricarica in background di secondi: %1</translation>
+    </message>
+    <message>
+        <source>Background auto-update interval, in seconds</source>
+        <translation>Intervallo auto ricarica in background di secondi</translation>
+    </message>
+    <message>
+        <source>Show subscription notifications in Events</source>
+        <translation>Mostra le notifiche per gli utenti selezionati nella schermata Eventi</translation>
+    </message>
+    <message>
+        <source>If disabled, subscription notifications will only be briefly shown and you won&apos;t see them in the Events view.</source>
+        <translation>Se disabilitato, le notifiche saranno solo mostrate brevemente e non le vedrai nella schermata Eventi.</translation>
+    </message>
+    <message>
+        <source>Reset subscriptions</source>
+        <translation>Ripristina sottoscrizioni</translation>
+    </message>
+    <message>
+        <source>Once a player you are subcribed to goes online, you will receive a notification. You are currently subscribed to the following players: %1.</source>
+        <translation>Una volta che un giocatore che hai scelto di seguire andrà in linea, riceverai una notifica. Attualmente stai seguendo i seguenti giocatori: %1.</translation>
+    </message>
+    <message>
+        <source>Show subscription notifications when initially loading</source>
+        <translation>Mostra notifiche di sottoscrizione al caricamento iniziale</translation>
+    </message>
+    <message>
+        <source>Show notifications for subscribed players when first starting the app or after toggling the platform.</source>
+        <translation>Mostra le notifiche per i giocatori seguiti non appena avvii l&apos;app o al cambio di piattaforma.</translation>
+    </message>
+    <message>
+        <source>%1 subscriptions reset</source>
+        <comment>%1 is the platform (e.g. DCNet or Dreamcast Now); reset is in past form</comment>
+        <translation>%1 ripristino sottoscrizioni</translation>
+    </message>
+</context>
+<context>
+    <name>harbour-dreamcastnow-client</name>
+    <message>
+        <source>%1 is now playing %2</source>
+        <comment>notification</comment>
+        <translation>%1 sta giocando adesso a %2</translation>
+    </message>
+    <message>
+        <source>%1 is now online</source>
+        <comment>notification</comment>
+        <translation>%1 è adesso in linea</translation>
+    </message>
+</context>
+</TS>
